4094fc111d3d90e74c1b6ab48bae959f6d3c8651
lichaosuns <1741721630@qq.com>
2024-01-04 23:01:31 +0800

    [fix]: 冒泡
    [body]:
    冒泡

---
+++ src/main/java/sort/BubbleSort.java
@@ -0,0 +1,41 @@
+package sort;
+
+/**
+ * 两两交换，适合交换次数少的有序数组
+ */
+public class BubbleSort {
+
+    public static void sort(int[] arr) {
+
+        boolean swap = false;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j + 1] <= arr[j]) {
+                    swap(arr, j + 1, j);
+                    swap = true;
+                }
+            }
+            //优化
+            if(!swap) {
+                System.out.println("----" + i);
+                break;
+            }
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{5, 4, 2, 1, 8, 9};
+        sort(arr);
+        for (int i : arr) {
+            System.out.println(i);
+        }
+
+        sort(arr);
+    }
+}
